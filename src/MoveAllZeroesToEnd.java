cd42adf8bdbbe997e225855a415d6d8b278750e9
Anuja <anujajadhav0704@gmail.com>
2025-04-18 21:06:53 +0530

    DAY 2 - Move All Zeroes to End - Easy

---
+++ src/MoveAllZeroesToEnd.java
@@ -0,0 +1,21 @@
+/*
+Move All Zeroes to End - Easy
+TC: O(N)
+SC: O(1)
+*/
+class MoveAllZeroesToEnd {
+    void pushZerosToEnd(int[] arr) {
+        // code here
+        //a pointer for last nonzero element
+        int n = arr.length;
+        int j = 0;
+        for(int i=0; i<n; i++){
+            if(arr[i] != 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+    }
+}
